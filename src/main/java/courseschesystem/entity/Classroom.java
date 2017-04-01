1d1a73552f818831e9371e0dbe7a4a98da505fbe
minesoul <1092226353@qq.com>
2017-04-01 23:28:37 +0800

    update

--- src/main/java/courseschesystem/entity/Classroom.java
+++ src/main/java/courseschesystem/entity/Classroom.java
@@ -1,10 +1,34 @@
 package courseschesystem.entity;
 
 /**
- * @Author: zzh
- * @Description:
- * @Date: Created in 9:36 2017/4/1
- * @Modified By:
+ * Created by Lily on 2017/3/31.
  */
 public class Classroom {
+    String rid;
+    String rname;
+    int seatnum;
+
+    public String getRid() {
+        return rid;
+    }
+
+    public void setRid(String rid) {
+        this.rid = rid;
+    }
+
+    public String getRname() {
+        return rname;
+    }
+
+    public void setRname(String rname) {
+        this.rname = rname;
+    }
+
+    public int getSeatnum() {
+        return seatnum;
+    }
+
+    public void setSeatnum(int seatnum) {
+        this.seatnum = seatnum;
+    }
 }
