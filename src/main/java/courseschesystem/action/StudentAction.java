84fb6f1c2856a181b181139281fff9d7d64a9f9f
minesoul <1092226353@qq.com>
2017-04-01 12:10:49 +0800

    update StudentAction.java

--- src/main/java/courseschesystem/action/StudentAction.java
+++ src/main/java/courseschesystem/action/StudentAction.java
@@ -1,36 +1,23 @@
 package courseschesystem.action;
 
+import com.opensymphony.xwork2.ModelDriven;
 import courseschesystem.entity.Student;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by zzh on 2017/3/29.
  */
-public class StudentAction {
+public class StudentAction extends SuperAction implements ModelDriven<List<Student>>{
 
-    public String queryProfessionSchedule(Student student){
-        /**
-         * @Author:
-         * @Description: 查看本专业推荐课表
-         * @param student
-         * @output: return "queryProfessionSchedule_success" or "queryProfessionSchedule_failure"
-         * @Date: Created in 21:56 2017/3/31
-         * @Modified By:
-         */
+    private static final long serialVersionUID = 1L;
+    private List<Student> students = new ArrayList<>();
 
+    @Override
+    public List<Student> getModel() {
 
-        return "";
+        return this.students;
     }
 
-    public String queryMySchedule(Student student){
-        /**
-         * @Author:
-         * @Description: 查看个人课表
-         * @param student
-         * @output: return "queryMySchedule_success" or "queryMySchedule_failure"
-         * @Date: Created in 9:35 2017/4/1
-         * @Modified By:
-         */
-
-        return "";
-    }
 }
