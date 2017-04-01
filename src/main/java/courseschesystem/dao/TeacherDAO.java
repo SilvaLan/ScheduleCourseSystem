9506bc64fe1457c6faec688f964fdb6b4f63ae6c
minesoul <1092226353@qq.com>
2017-04-02 00:05:14 +0800

    update

--- src/main/java/courseschesystem/dao/TeacherDAO.java
+++ src/main/java/courseschesystem/dao/TeacherDAO.java
@@ -1,10 +1,19 @@
 package courseschesystem.dao;
 
+import courseschesystem.entity.Teacher;
+
+import java.util.List;
+
 /**
- * @Author: zzh
- * @Description:
- * @Date: Created in 16:59 2017/3/30
+ * @Author: Lily
+ * @Description: Teacher的接口类
+ * @Date: Created in 10:46 2017/4/1
  * @Modified By:
  */
 public interface TeacherDAO {
+    boolean addTeacher(Teacher teacher);
+    boolean deleteTeacher(Teacher teacher);
+    boolean updateTeacher(Teacher teacher);
+    List<Teacher> queryTeacher(Teacher teacher);
+
 }
