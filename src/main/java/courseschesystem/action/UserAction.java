d38f85e4f1b6f9e7db292f06d8c996f4b553c808
minesoul <1092226353@qq.com>
2017-04-01 13:02:52 +0800

    update

--- src/main/java/courseschesystem/action/UserAction.java
+++ src/main/java/courseschesystem/action/UserAction.java
@@ -1,35 +1,36 @@
 package courseschesystem.action;
 
+import com.opensymphony.xwork2.ModelDriven;
 import courseschesystem.entity.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by zzh on 2017/3/29.
  */
-public class UserAction {
+public class UserAction extends SuperAction implements ModelDriven<List<User>>{
 
-    public String login(User user){
-        /**
-         * @Author: zzh
-         * @Description: User登录验证
-         * @param user
-         * @output: if user exists, return the user's type as "student_type", "teacher_type". "manager_type", else return "login_failure"
-         * @Date: Created in 18:26 2017/3/30
-         * @Modified By:
-         */
+    private static final long serialVersionUID = 1L;
+    private List<User> users = new ArrayList<>();
 
-        return "";
+    @Override
+    public List<User> getModel() {
+
+        return this.users;
     }
 
-    public String checkUserType(User user){
+    public String login(String uid, String upwd,String utype){
         /**
          * @Author: zzh
-         * @Description: 获取用户类型
-         * @param user
-         * @output: return user's type
-         * @Date: Created in 18:35 2017/3/30
+         * @Description: 根据用户名、密码、用户类型，实现登录校验
+         * @param uid
+         * @param upwd
+         * @param utype
+         * @output:
+         * @Date: Created in 12:26 2017/4/1
          * @Modified By:
          */
-
 
         return "";
     }
