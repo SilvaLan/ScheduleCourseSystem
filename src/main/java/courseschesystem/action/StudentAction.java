fb33f909c6e284cf697b1feef77cc41f619ddbb3
minesoul <1092226353@qq.com>
2017-04-01 11:24:51 +0800

    update StudentAction.java

--- src/main/java/courseschesystem/action/StudentAction.java
+++ src/main/java/courseschesystem/action/StudentAction.java
@@ -9,11 +9,25 @@ public class StudentAction {
 
     public String queryProfessionSchedule(Student student){
         /**
-         * @Author: zzh
-         * @Description:
+         * @Author:
+         * @Description: 查看本专业推荐课表
          * @param student
-         * @output:
+         * @output: return "queryProfessionSchedule_success" or "queryProfessionSchedule_failure"
          * @Date: Created in 21:56 2017/3/31
+         * @Modified By:
+         */
+
+
+        return "";
+    }
+
+    public String queryMySchedule(Student student){
+        /**
+         * @Author:
+         * @Description: 查看个人课表
+         * @param student
+         * @output: return "queryMySchedule_success" or "queryMySchedule_failure"
+         * @Date: Created in 9:35 2017/4/1
          * @Modified By:
          */
 
