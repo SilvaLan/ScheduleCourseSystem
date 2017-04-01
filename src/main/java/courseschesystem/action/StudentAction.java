33222dad799233a4060f3c13d2b96e465dcbd4d6
minesoul <1092226353@qq.com>
2017-04-02 02:30:12 +0800

    update

--- src/main/java/courseschesystem/action/StudentAction.java
+++ src/main/java/courseschesystem/action/StudentAction.java
@@ -20,6 +20,32 @@ public class StudentAction extends SuperAction implements ModelDriven<List<Stude
         return this.students;
     }
 
+    public String getStudentNumByMajor(int total,String majorID){
+        /**
+         * @Author: zzh
+         * @Description: 统计本专业学生总数
+         * @param total
+         * @param majorID 专业号
+         * @output: return "getStudentNumByMajor_success" if count success, or return "getStudentNumByMajor_success"
+         * @Date: Created in 2:23 2017/4/2
+         * @Modified By:
+         */
 
+        return "";
+    }
+
+    public String getStudentNumByCourse(int num,String cno){
+        /**
+         * @Author: zzh
+         * @Description: 统计上某门课的学生的总数
+         * @param num
+         * @param cno 课程号
+         * @output: return "getStudentNumByMCourse_success" if count success, or return "getStudentNumByCourse_success"
+         * @Date: Created in 2:26 2017/4/2
+         * @Modified By:
+         */
+
+        return "";
+    }
 
 }
