e38b2df4e75c3210c5c39e97d81b2566d24c6026
minesoul <1092226353@qq.com>
2017-04-02 00:32:25 +0800

    update

--- src/main/java/courseschesystem/service/impl/UserServiceImpl.java
+++ src/main/java/courseschesystem/service/impl/UserServiceImpl.java
@@ -1,22 +1,30 @@
 package courseschesystem.service.impl;
 
+import courseschesystem.dao.UserDAO;
+import courseschesystem.dao.impl.UserDAOImpl;
 import courseschesystem.entity.User;
+import courseschesystem.service.UserService;
+
+import java.util.List;
 
 /**
  * Created by zzh on 2017/3/29.
  */
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService{
 
-    public boolean loginQuery(User user){
+
+    public boolean login(User user){
         /**
          * @Author: zzh
-         * @Description: 进行用户查询，若用户存在，则登录成功，否则失败
+         * @Description: 传入User对象，校验User对象信息是否正确，返回校验结果
          * @param user
-         * @output: return true if user exist, else return false
-         * @Date: Created in 18:29 2017/3/30
+         * @output: if user exists,return true, else return false
+         * @Date: Created in 12:29 2017/4/1
          * @Modified By:
          */
 
-        return true;
+        UserDAO userDAO = new UserDAOImpl();
+        List<User> users = userDAO.queryUser(user);
+        return users != null;
     }
 }
