446858a9ea531ee14b1ea94d7f640b11f73acd45
minesoul <1092226353@qq.com>
2017-04-01 23:28:02 +0800

    update

--- src/main/java/courseschesystem/entity/Student.java
+++ src/main/java/courseschesystem/entity/Student.java
@@ -1,8 +1,117 @@
 package courseschesystem.entity;
 
+import java.util.Date;
+
 /**
- * Created by zzh on 2017/3/29.
+ * Created by Lily on 2017/3/31.
  */
 public class Student {
+    String stuid;
+    String stuname;
+    String stusex;
+    Date stubirth;
+    String stuIDnum;
+    String sturace;
+    Date stuindate;
+    String classid;
+    String majorid;
+    String aid;
+    String mail;
+    String edu;
+
+    public String getStuid() {
+        return stuid;
+    }
+    public void setStuid(String stuid) {
+        this.stuid = stuid;
+    }
+
+    public String getStuname() {
+        return stuname;
+    }
+
+    public void setStuname(String stuname) {
+        this.stuname = stuname;
+    }
+
+    public String getStusex() {
+        return stusex;
+    }
+
+    public void setStusex(String stusex) {
+        this.stusex = stusex;
+    }
+
+    public Date getStubirth() {
+        return stubirth;
+    }
+
+    public void setStubirth(Date stubirth) {
+        this.stubirth = stubirth;
+    }
+
+    public String getStuIDnum() {
+        return stuIDnum;
+    }
+
+    public void setStuIDnum(String stuIDnum) {
+        this.stuIDnum = stuIDnum;
+    }
+
+    public String getSturace() {
+        return sturace;
+    }
+
+    public void setSturace(String sturace) {
+        this.sturace = sturace;
+    }
+
+    public Date getStuindate() {
+        return stuindate;
+    }
+
+    public void setStuindate(Date stuindate) {
+        this.stuindate = stuindate;
+    }
+
+    public String getClassid() {
+        return classid;
+    }
+
+    public void setClassid(String classid) {
+        this.classid = classid;
+    }
+
+    public String getMajorid() {
+        return majorid;
+    }
+
+    public void setMajorid(String majorid) {
+        this.majorid = majorid;
+    }
+
+    public String getAid() {
+        return aid;
+    }
+
+    public void setAid(String aid) {
+        this.aid = aid;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getEdu() {
+        return edu;
+    }
+
+    public void setEdu(String edu) {
+        this.edu = edu;
+    }
 
 }
