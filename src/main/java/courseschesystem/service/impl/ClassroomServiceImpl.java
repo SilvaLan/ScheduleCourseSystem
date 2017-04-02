9ff7479d885c188d575820fdd1beb170b6268220
minesoul <1092226353@qq.com>
2017-04-02 12:19:56 +0800

    update

--- src/main/java/courseschesystem/service/impl/ClassroomServiceImpl.java
+++ src/main/java/courseschesystem/service/impl/ClassroomServiceImpl.java
@@ -1,7 +1,26 @@
 package courseschesystem.service.impl;
 
+import courseschesystem.entity.Classroom;
+
+import java.util.Date;
+import java.util.List;
+
 /**
  * Created by zzh on 2017/3/29.
  */
 public class ClassroomServiceImpl {
+
+    public List<Classroom> queryClassroomByDate(Date date){
+        /**
+         * @Author: zzh
+         * @Description: 根据日期查询教室使用情况
+         * @param date 日期
+         * @output: 返回教室使用信息，存储在List中
+         * @Date: Created in 11:23 2017/4/2
+         * @Modified By:
+         */
+
+        return null;
+    }
+
 }
