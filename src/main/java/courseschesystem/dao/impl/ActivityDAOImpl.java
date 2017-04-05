359fa98c296be7c7691870c7981c60dcfe0960e6
minesoul <1092226353@qq.com>
2017-04-05 19:29:29 +0800

    更新

---
+++ src/main/java/courseschesystem/dao/impl/ActivityDAOImpl.java
@@ -0,0 +1,47 @@
+package courseschesystem.dao.impl;
+
+import courseschesystem.dao.ActivityDAO;
+import courseschesystem.entity.Activity;
+
+import java.util.List;
+
+/**
+ * @Author: zzh
+ * @Description:
+ * @Date: Created in 19:21 2017/4/5
+ * @Modified By:
+ */
+public class ActivityDAOImpl implements ActivityDAO{
+
+    /**
+     * @Author:
+     * @Description: add 增加
+     * @param
+     * @output:
+     * @Date: Created in 19:24 2017/4/5
+     * @Modified By:
+     */
+
+    /**
+     * @Author:
+     * @Description: delete 删除
+     * @param
+     * @output:
+     * @Date: Created in 19:24 2017/4/5
+     * @Modified By:
+     */
+
+    public List<Activity> queryActivity(Activity activity){
+        /**
+         * @Author:
+         * @Description: 查找
+         * @param activity
+         * @output:
+         * @Date: Created in 19:27 2017/4/5
+         * @Modified By:
+         */
+
+        return null;
+    }
+
+}
